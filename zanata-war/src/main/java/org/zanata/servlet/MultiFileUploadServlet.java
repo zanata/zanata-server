308bfc35bd905ad6820c01b0d75580405c70bf9e
David Mason <dr.d.mason@gmail.com>
2014-05-20 09:55:02 +1000

    WIP: getting json libraries working properly

--- zanata-war/src/main/java/org/zanata/servlet/MultiFileUploadServlet.java
+++ zanata-war/src/main/java/org/zanata/servlet/MultiFileUploadServlet.java
@@ -28,6 +28,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map.Entry;
 
+import javax.persistence.OptimisticLockException;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -40,10 +41,12 @@ import org.apache.commons.fileupload.FileItemFactory;
 import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.hibernate.StaleStateException;
 import org.jboss.seam.Component;
 import org.jboss.seam.servlet.ContextualHttpServletRequest;
-//import org.json.JSONArray;
-//import org.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.zanata.file.GlobalDocumentId;
@@ -52,6 +55,8 @@ import org.zanata.rest.DocumentFileUploadForm;
 import org.zanata.rest.dto.ChunkUploadResponse;
 import org.zanata.service.TranslationFileService;
 import org.zanata.service.impl.TranslationFileServiceImpl;
+
+import static com.google.common.base.Strings.isNullOrEmpty;
 
 /**
  * Endpoint for upload dialogs using multi-file upload forms.
@@ -106,68 +111,76 @@ public class MultiFileUploadServlet extends HttpServlet {
         String projectSlug = request.getParameter("p");
         String versionSlug = request.getParameter("v");
 
-        Enumeration<String> paramNames = request.getParameterNames();
-
-        while (paramNames.hasMoreElements()) {
-            log.info("param: " + paramNames.nextElement());
-        }
-
-        log.info("project-version: " + projectSlug + ":" + versionSlug);
 
         // Create a factory for disk-based file items
         FileItemFactory factory = new DiskFileItemFactory();
         ServletFileUpload uploadHandler = new ServletFileUpload(factory);
 
-        PrintWriter writer = response.getWriter();
         response.setContentType("application/json");
-        JSONArray filesJson = new JSONArray();
+        PrintWriter writer = response.getWriter();
+
+        List<FileItem> items;
         try {
-            List<FileItem> items = uploadHandler.parseRequest(request);
-
-            String path = "";
-            String lang = "en-US";
-            String fileParams = "";
-
-            // Make sure params are available before processing files
-            for (FileItem item : items) {
-                if (item.isFormField()) {
-                    String field = item.getFieldName();
-                    String value = item.getString();
-                    if (field.equals("filepath")) {
-                        path = value;
-                    } else if (field.equals("filelang")) {
-                        lang = value;
-                    } else if (field.equals("fileparams")) {
-                        fileParams = value;
-                    }
-                }
-            }
-
-            for (FileItem item : items) {
-                if (!item.isFormField()) {
-                    JSONObject jsono =
-                            processFileItem(item, projectSlug, versionSlug,
-                                    path, fileParams);
-                    filesJson.put(jsono);
-                }
-            }
+            items = uploadHandler.parseRequest(request);
         } catch (FileUploadException e) {
-                throw new RuntimeException(e);
-        } catch (Exception e) {
-                throw new RuntimeException(e);
-        } finally {
             JSONObject responseObject = new JSONObject();
-            responseObject.put("files", filesJson);
-            String responseString = responseObject.toString();
-            log.info("response string: " + responseString);
-            writer.write(responseString);
+            try {
+                responseObject.put("error", "upload failed: " + e.getMessage());
+            } catch (JSONException je) {
+                log.error("Error while generating JSON", je);
+            }
+            writer.write(responseObject.toString());
             writer.close();
+            return;
         }
+
+        JSONArray filesJson = new JSONArray();
+
+        String path = "";
+        String lang = "en-US";
+        String fileParams = "";
+
+        // Make sure params are available before processing files
+        for (FileItem item : items) {
+            if (item.isFormField()) {
+                String field = item.getFieldName();
+                String value = item.getString();
+                if (field.equals("filepath")) {
+                    path = value;
+                } else if (field.equals("filelang")) {
+                    lang = value;
+                } else if (field.equals("fileparams")) {
+                    fileParams = value;
+                }
+            }
+        }
+
+        for (FileItem item : items) {
+            if (!item.isFormField()) {
+                JSONObject jsono =
+                        processFileItem(item, projectSlug, versionSlug,
+                                path, fileParams);
+                filesJson.put(jsono);
+            }
+        }
+        JSONObject responseObject = new JSONObject();
+        try {
+            responseObject.put("files", filesJson);
+        } catch (JSONException e) {
+            log.error("error adding files list to JSON", e);
+        }
+        String responseString = responseObject.toString();
+        log.info("response string: " + responseString);
+        writer.write(responseString);
+        writer.close();
+
+        // FIXME this needs to be very robust so it doesn't risk taking down the server
+        //       so I should really wrap it in a catch Exception that will print error
+        //       stack trace and return a response with error to the server.
     }
 
     public JSONObject processFileItem(FileItem item, String projectSlug,
-            String versionSlug, String path, String fileParams)
-            throws IOException {
+            String versionSlug, String path, String fileParams) {
         String docId = translationFileServiceImpl.generateDocId(path, item.getName());
 
         GlobalDocumentId id = new GlobalDocumentId(projectSlug, versionSlug, docId);
@@ -176,87 +189,48 @@ public class MultiFileUploadServlet extends HttpServlet {
         form.setFirst(true);
         form.setLast(true);
         form.setSize(item.getSize());
-        form.setFileStream(item.getInputStream());
         form.setFileType(translationFileServiceImpl.extractExtension(item.getName()));
 
-        Response resp = sourceUploader.tryUploadSourceFileWithoutHash(id , form);
-        ChunkUploadResponse responseEntity = (ChunkUploadResponse) resp.getEntity();
-
-        String error = responseEntity.getErrorMessage();
-        String success = responseEntity.getSuccessMessage();
+        String error;
+        String success = null;
+        try {
+            form.setFileStream(item.getInputStream());
+            Response resp = sourceUploader.tryUploadSourceFileWithoutHash(id, form);
+            ChunkUploadResponse responseEntity = (ChunkUploadResponse) resp.getEntity();
+            error = responseEntity.getErrorMessage();
+            success = responseEntity.getSuccessMessage();
+        } catch (IOException e) {
+            error = "could not access file data";
+        } catch (OptimisticLockException e) {
+            error = "failed: concurrent upload";
+        } catch (StaleStateException e) {
+            // this happens in the same circumstances as OptimisticLockException
+            // but is thrown because we are using hibernate directly rather than
+            // through JPA.
+            error = "failed: concurrent upload";
+        }
 
         JSONObject jsono = new JSONObject();
 
-        jsono.put("name", docId);
-        jsono.put("size", item.getSize());
-        if (error != null && !error.isEmpty()) {
-            if (error.equals("Valid combination of username and api-key for this server were not included in the request.")) {
-                error = "not logged in";
-            }
-            jsono.put("error", error);
-        } else {
-            if (success != null && !success.isEmpty()) {
-                jsono.put("message", success);
-            }
-            // TODO could provide REST URL for this file
+        try {
+            jsono.put("name", docId);
+            jsono.put("size", item.getSize());
+            if (!isNullOrEmpty(error)) {
+                if (error.equals("Valid combination of username and api-key for this server were not included in the request.")) {
+                    error = "not logged in";
+                }
+                jsono.put("error", error);
+            } else {
+                if (!isNullOrEmpty(success)) {
+                    jsono.put("message", success);
+                }
+                // TODO could provide REST URL for this file
 //                            jsono.put("url", "upload?getfile=" + item.getName());
+            }
+        } catch (JSONException e) {
+            log.error("Error while generating JSON", e);
         }
         return jsono;
-    }
-
-
-    // FIXME maven is having some issue with using org.json here,
-    //       so these are just some quick replica classes to get things working
-    //       until I have time to sort out the dependencies.
-    //       Are these coming from gwt sdk and so not available?
-    private static class JSONObject extends HashMap<String, String> {
-
-        @Override
-        public String put(String key, String value) {
-            return super.put(key, "\"" + value.replace("\"", "\\\"") + "\"");
-        }
-
-        public void put(String key, long value) {
-            super.put(key, new Long(value).toString());
-        }
-
-        public void put(String key, JSONArray value) {
-            super.put(key, value.toString());
-        }
-
-        @Override
-        public String toString() {
-            boolean first = true;
-            String value = "{\n    ";
-            for (Entry<String, String> e : entrySet()) {
-                value += first ? "" : ",\n    ";
-                first = false;
-                value += "\"" + e.getKey() + "\": ";
-                value += e.getValue();
-            }
-            value += "\n  }";
-            return value;
-        }
-    }
-
-    private static class JSONArray extends ArrayList<String> {
-
-        public void put(JSONObject jsono) {
-            add(jsono.toString());
-        }
-
-        @Override
-        public String toString() {
-            boolean first = true;
-            String output = "[\n  ";
-            for (String s : this) {
-                output += first ? "" : ",\n  ";
-                first = false;
-                output += s;
-            }
-            output += "\n]";
-            return output;
-        }
     }
 
 }
