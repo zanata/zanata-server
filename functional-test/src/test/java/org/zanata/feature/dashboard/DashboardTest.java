d4d584282f3ab5dd3fab12b98ba592dcd9014002
Carlos A. Munoz <camunoz@redhat.com>
2014-07-03 15:46:25 +1000

    Refactor test to avoid random timing-related failures.

--- functional-test/src/test/java/org/zanata/feature/dashboard/DashboardTest.java
+++ functional-test/src/test/java/org/zanata/feature/dashboard/DashboardTest.java
@@ -91,8 +91,7 @@ public class DashboardTest extends ZanataTestCase {
         DashboardActivityTab activityTab = dashboard.gotoActivityTab();
         assertThat(activityTab.getMyActivityList()).isNotEmpty();
         int initialActivitySize = activityTab.getMyActivityList().size();
-        activityTab.clickMoreActivity();
-        return activityTab.getMyActivityList().size() > initialActivitySize;
+        return activityTab.clickMoreActivity();
     }
 
     private boolean projectListIsNotEmpty() throws Exception {
