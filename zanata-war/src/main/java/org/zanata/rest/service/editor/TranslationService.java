2ebc7b63d57967a328019cee12ac20c7ae3ee3e7
Alex Eng <aeng@redhat.com>
2014-10-01 15:07:04 +1000

    Add translation endpoint

--- zanata-war/src/main/java/org/zanata/rest/service/editor/TranslationService.java
+++ zanata-war/src/main/java/org/zanata/rest/service/editor/TranslationService.java
@@ -20,58 +20,46 @@
  */
 package org.zanata.rest.service.editor;
 
-import java.lang.reflect.Type;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
-import javax.annotation.Nullable;
 import javax.ws.rs.Path;
-import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.Response;
 
 import org.apache.commons.lang.StringUtils;
-import org.jboss.resteasy.util.GenericType;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Transactional;
 import org.zanata.common.LocaleId;
 import org.zanata.dao.TextFlowDAO;
-import org.zanata.model.HLocale;
+import org.zanata.dao.TextFlowTargetDAO;
 import org.zanata.model.HTextFlow;
-import org.zanata.rest.dto.Locale;
+import org.zanata.model.HTextFlowTarget;
 import org.zanata.rest.dto.resource.TextFlow;
+import org.zanata.rest.dto.resource.TextFlowTarget;
 import org.zanata.rest.dto.resource.TransUnit;
 import org.zanata.rest.dto.resource.TransUnits;
 import org.zanata.rest.service.ResourceUtils;
-import org.zanata.service.LocaleService;
 
-import com.google.common.base.Function;
-import com.google.common.base.Predicate;
-import com.google.common.collect.Collections2;
-import com.google.common.collect.FluentIterable;
+import com.google.common.base.Optional;
 import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
 
 /**
  * @author Alex Eng <a href="mailto:aeng@redhat.com">aeng@redhat.com</a>
  */
-@Name("sourceService")
-@Path(SourceResource.SERVICE_PATH)
+@Name("translationService")
+@Path(TranslationResource.SERVICE_PATH)
 @Transactional
-public class SourceService implements SourceResource {
+public class TranslationService implements TranslationResource {
     public static int MAX_ID_SIZE = 200;
 
     @In
-    private TextFlowDAO textFlowDAO;
+    private TextFlowTargetDAO textFlowTargetDAO;
 
     @In
     private ResourceUtils resourceUtils;
 
     @Override
-    public Response get(String ids) {
+    public Response get(String localeId, String ids) {
         TransUnits transUnits = new TransUnits();
         if (StringUtils.isEmpty(ids)) {
             return Response.ok(transUnits).build();
@@ -81,20 +69,26 @@ public class SourceService implements SourceResource {
             return Response.status(Response.Status.FORBIDDEN).build();
         }
 
-        List<Long> filteredIds = Lists.newArrayList();
+        LocaleId locale = new LocaleId(localeId);
+
         for(String id: idList) {
             if(StringUtils.isNumeric(id)) {
-                filteredIds.add(Long.parseLong(id));
+                HTextFlowTarget hTarget = textFlowTargetDAO.getTextFlowTarget(
+                        Long.parseLong(id), locale);
+                if(hTarget != null) {
+                    HTextFlow htf = hTarget.getTextFlow();
+                    TextFlow tf = new TextFlow(htf.getResId(), locale);
+                    resourceUtils.transferToTextFlow(htf, tf);
+
+                    TextFlowTarget target = new TextFlowTarget(htf.getResId());
+                    resourceUtils.transferToTextFlowTarget(hTarget, target,
+                            Optional.<String>absent());
+
+                    TransUnit tu = new TransUnit();
+                    tu.addTarget(locale, target);
+                    transUnits.addTransUnit(htf.getId().toString(), tu);
+                }
             }
-        }
-
-        List<HTextFlow> hTextFlows = textFlowDAO.findByIdList(filteredIds);
-
-        for(HTextFlow htf: hTextFlows) {
-            LocaleId localeId = htf.getDocument().getLocale().getLocaleId();
-            TextFlow tf = new TextFlow(htf.getResId(), localeId);
-            resourceUtils.transferToTextFlow(htf, tf);
-            transUnits.addTransUnit(htf.getId().toString(), new TransUnit(tf));
         }
         return Response.ok(transUnits).build();
     }
