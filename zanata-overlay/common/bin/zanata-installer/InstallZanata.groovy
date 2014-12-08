fc5daf1bf29b484e5a4e41c7d71f728f4c2c2b6b
Carlos A. Munoz <camunoz@redhat.com>
2014-12-08 16:19:19 +1000

    Make sure the installation process is repeatable if there is a failure.
    Also, downgraded the mysql driver version as the one used before did not deploy on wildfly.

--- zanata-overlay/common/bin/zanata-installer/InstallZanata.groovy
+++ zanata-overlay/common/bin/zanata-installer/InstallZanata.groovy
@@ -19,20 +19,26 @@ final CUSTOM_STANDALONE_XML_LOC = "${JBOSS_CFG_DIR}/standalone-zanata.xml"
 final ORIGINAL_STANDALONE_XML_LOC = "${JBOSS_CFG_DIR}/standalone.xml"
 final WAR_FILE_LOC = "../../standalone/deployments/zanata.war"
 
-if( new File(WAR_FILE_LOC).exists() ) {
-    println "It seems Zanata is already installed. If you wish to upgrade Zanata " +
-        "you need to download a more recent web archive file and drop it in the " +
-        "standalone/deployments directory of your Zanata server installation."
-    return
+boolean askYesNoQuestion(String question) {
+    def reader = new BufferedReader(new InputStreamReader(System.in))
+    println ""
+    println question
+    def answer = reader.readLine()
+    return answer.trim().equalsIgnoreCase("y")
 }
 
 println "====================================================="
 println " Welcome to the Zanata Installer. This will install"
 println " Zanata ${ZANATA_VERSION} onto your JBoss or Wildfly"
 println ""
-println " Note: This installer will change your standalone.xml" +
+println " Note: This installer will change your standalone.xml"
 println " file."
 println "====================================================="
+
+def downloadWarFile
+if( new File(WAR_FILE_LOC).exists() ) {
+    downloadWarFile = askYesNoQuestion("It seems Zanata is already installed. Do you wish to download it again?")
+}
 
 def dbHost
 def dbPort
@@ -40,11 +46,41 @@ def dbSchema
 def dbUsername
 def dbPassword
 
-println "Please provide the following information:"
-println "(If blank, the default value will apply)"
-println ""
+// Download the war file
+if( downloadWarFile ) {
+    def fileUrl = "http://sourceforge.net/projects/zanata/files/webapp/zanata-war-${ZANATA_VERSION}.war/download"
+    println "Downloading $fileUrl"
+    println "This might take a few minutes."
 
-System.in.withReader { reader ->
+    try {
+        def file = new FileOutputStream(WAR_FILE_LOC)
+        def out = new BufferedOutputStream(file)
+        out << new URL(fileUrl).openStream()
+        out.close()
+    } catch (Exception ex) {
+        println "Could not download zanata-war-${ZANATA_VERSION}.war"
+    }
+}
+
+// Move the original standalone and copy the custom one
+def xmlFile = new File(ORIGINAL_STANDALONE_XML_LOC)
+def xmlBackupFile = new File("${JBOSS_CFG_DIR}/standalone.xml.original")
+def customXmlFile = new File(CUSTOM_STANDALONE_XML_LOC)
+
+// If the installer has already been ran
+def modifyStandaloneXml = true
+if(xmlBackupFile.exists()) {
+    modifyStandaloneXml = askYesNoQuestion("Looks like you've previosuly ran this installer. " +
+        "If you continue, your current standalone.xml file will be backed up and modified. " +
+        "Do you wish to continue? (y/N)")
+}
+
+if(modifyStandaloneXml) {
+    println "Please provide the following information:"
+    println "(If blank, the default value will apply)"
+    println ""
+
+    def reader = new BufferedReader(new InputStreamReader(System.in))
 
     println "Database Host (default: 'localhost'): "
     dbHost = reader.readLine()
@@ -63,43 +99,31 @@ System.in.withReader { reader ->
 
     println "Database Password: (default: '')"
     dbPassword = reader.readLine()
-}
 
-def fileUrl = "http://sourceforge.net/projects/zanata/files/webapp/zanata-war-${ZANATA_VERSION}.war/download"
-println "Downloading $fileUrl"
-println "This might take a few minutes."
+    xmlFile.renameTo(xmlBackupFile) // backup original file
+    Files.copy(customXmlFile.toPath(), xmlFile.toPath()) // standalone-zanata.xml -> standalone.xml
 
-try {
-    def file = new FileOutputStream(WAR_FILE_LOC)
-    def out = new BufferedOutputStream(file)
-    out << new URL(fileUrl).openStream()
-    out.close()
-} catch (Exception ex) {
-    println "Could not download zanata-war-${ZANATA_VERSION}.war"
-}
+    // Update zanata datasource
+    def dsXml = new XmlParser().parse(CUSTOM_STANDALONE_XML_LOC)
 
-// Move the original standalone and copy the custom one
-def xmlFile = new File(ORIGINAL_STANDALONE_XML_LOC)
-def xmlBackupFile = new File("${JBOSS_CFG_DIR}/standalone.xml.original")
-def customXmlFile = new File(CUSTOM_STANDALONE_XML_LOC)
-xmlFile.renameTo(xmlBackupFile)
-Files.copy(xmlBackupFile.toPath(), customXmlFile.toPath())
+    def zanataDs =
+        dsXml.profile.subsystem.
+            find { s -> s.datasources }.datasources.datasource
+            .find {
+            it.@'jndi-name' == "java:jboss/datasources/zanataDatasource"
+        }
 
-// Update zanata datasource
-def dsXml = new XmlParser().parse(CUSTOM_STANDALONE_XML_LOC)
-
-def zanataDs =
-    dsXml.profile.subsystem.find { s -> s.datasources }.datasources.datasource
-        .find { it.@'jndi-name' == "java:jboss/datasources/zanataDatasource" }
-
-zanataDs.'connection-url'.replaceNode {
-    'connection-url'( "jdbc:mysql://${dbHost}:${dbPort}/${dbSchema}?characterEncoding=UTF-8" )
-}
-zanataDs.security.replaceNode { node ->
-    security {
-        'user-name'( dbUsername )
-        password( dbPassword )
+    zanataDs.'connection-url'.replaceNode {
+        'connection-url'(
+            "jdbc:mysql://${dbHost}:${dbPort}/${dbSchema}?characterEncoding=UTF-8")
     }
-}
+    zanataDs.security.replaceNode { node ->
+        security {
+            'user-name'(dbUsername)
+            password(dbPassword)
+        }
+    }
 
-println XmlUtil.serialize(dsXml, new FileOutputStream(CUSTOM_STANDALONE_XML_LOC))
+    println XmlUtil.
+        serialize(dsXml, new FileOutputStream(CUSTOM_STANDALONE_XML_LOC))
+}
