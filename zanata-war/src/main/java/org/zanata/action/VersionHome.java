c0013ab58852756f004486ec923187e6d4fb162d
Carlos A. Munoz <camunoz@redhat.com>
2014-12-18 10:30:10 +1000

    Replace the use of seam's jsf messages wrapper.
    Create Zanata's own wrapper, which may be replaced later with a CDI extension if needed.

--- zanata-war/src/main/java/org/zanata/action/VersionHome.java
+++ zanata-war/src/main/java/org/zanata/action/VersionHome.java
@@ -37,7 +37,6 @@ import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.security.Restrict;
 import org.jboss.seam.core.Events;
-import org.jboss.seam.faces.FacesMessages;
 import org.zanata.common.EntityStatus;
 import org.zanata.common.LocaleId;
 import org.zanata.common.ProjectType;
@@ -52,6 +51,7 @@ import org.zanata.service.LocaleService;
 import org.zanata.service.SlugEntityService;
 import org.zanata.service.ValidationService;
 import org.zanata.ui.autocomplete.LocaleAutocomplete;
+import org.zanata.ui.faces.FacesMessages;
 import org.zanata.util.ComparatorUtil;
 import org.zanata.webtrans.shared.model.ValidationAction;
 import org.zanata.webtrans.shared.model.ValidationId;
@@ -83,6 +83,9 @@ public class VersionHome extends SlugHome<HProjectIteration> {
     @Getter
     @Setter
     private String projectSlug;
+
+    @In("jsfMessages")
+    private FacesMessages facesMessages;
 
     @In
     private ProjectIterationDAO projectIterationDAO;
@@ -291,7 +294,7 @@ public class VersionHome extends SlugHome<HProjectIteration> {
 
     public boolean validateSlug(String slug, String componentId) {
         if (!isSlugAvailable(slug)) {
-            FacesMessages.instance().addToControl(componentId,
+            facesMessages.addToControl(componentId,
                     "This Version ID has been used in this project");
             return false;
         }
