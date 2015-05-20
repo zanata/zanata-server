6846482c0f6fce18025167a4554bd442da2f98ed
Sean Flanigan <sflaniga@redhat.com>
2015-05-20 18:07:21 +1000

    Ensure that SeamAutowire loads before tests

--- zanata-war/src/test/java/org/zanata/testng/TestObjectFactory.java
+++ zanata-war/src/test/java/org/zanata/testng/TestObjectFactory.java
@@ -1,7 +1,30 @@
-package org;
+/*
+ * Copyright 2015, Red Hat, Inc. and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.zanata.testng;
 
+import org.testng.IObjectFactory;
 import org.zanata.seam.SeamAutowire;
-import com.google.common.base.Throwables;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 
 /**
  * This class is to make sure SeamAutowire is loaded earlier. If class
@@ -50,13 +73,20 @@ import com.google.common.base.Throwables;
  *
  * @author Patrick Huang <a
  *         href="mailto:pahuang@redhat.com">pahuang@redhat.com</a>
+ * @author Sean Flanigan <a
+ *         href="mailto:sflaniga@redhat.com">sflaniga@redhat.com</a>
  */
-public class ForceLoadClass {
-    static {
+public class TestObjectFactory
+        implements IObjectFactory {
+    @Override
+    public Object newInstance(Constructor constructor, Object... params) {
+        // force Seam classes to be augmented before anyone loads the originals
+        SeamAutowire.instance();
         try {
-            Class.forName(SeamAutowire.class.getCanonicalName());
-        } catch (ClassNotFoundException e) {
-            throw Throwables.propagate(e);
+            return constructor.newInstance(params);
+        } catch (InstantiationException | IllegalAccessException
+                | InvocationTargetException e) {
+            throw new RuntimeException(e);
         }
     }
 }
