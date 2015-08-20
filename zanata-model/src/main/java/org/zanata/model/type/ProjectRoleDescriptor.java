452795fcca9c492b0e6c553875e23dcdbebeb030
David Mason <drdmason@gmail.com>
2015-08-20 10:09:00 +1000

    refactor: whitespace and comment wording changes

--- zanata-model/src/main/java/org/zanata/model/type/ProjectRoleDescriptor.java
+++ zanata-model/src/main/java/org/zanata/model/type/ProjectRoleDescriptor.java
@@ -18,7 +18,6 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-
 package org.zanata.model.type;
 
 import org.zanata.model.ProjectRole;
