2082345817e6cbcc8f008180f9e89f3ab04baedd
Alex Eng <aeng@redhat.com>
2015-10-13 19:58:53 +1000

    Add prod-debug config

---
+++ frontend/src/main/web/webpack.prod.config.debug.js
@@ -0,0 +1,56 @@
+var webpack = require('webpack');
+var path = require('path');
+
+// bundle destination (default is current directory)
+var bundleDest = process.env.npm_config_env_bundleDest || __dirname;
+
+module.exports = {
+  context: __dirname,
+  devtool: 'eval',
+  entry: [
+    './index'
+  ],
+  output: {
+    path: bundleDest,
+    filename: 'frontend.bundle.min.js',
+    pathinfo: true
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css', 'cssnext']
+      }
+    ]
+  },
+  cssnext: {
+    compress: true,
+    features: {
+      rem: false,
+      pseudoElements: false,
+      colorRgba: false
+    }
+  },
+  plugins: [
+    new webpack.DefinePlugin({ "global.GENTLY": false }),
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("development")
+      }
+    }),
+    new webpack.NoErrorsPlugin()
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.json', '.css']
+  },
+  node: {
+    __dirname: true
+  }
+};
