ed38343ecaf6b03c7f581336f59ade4002cabff1
Damian Jansen <djansen@redhat.com>
2015-10-16 13:43:07 +1000

    feat(document): Support plurals in Qt ts documents

--- zanata-war/src/main/java/org/zanata/adapter/TSAdapter.java
+++ zanata-war/src/main/java/org/zanata/adapter/TSAdapter.java
@@ -21,7 +21,24 @@
 
 package org.zanata.adapter;
 
+import com.google.common.base.Optional;
+import lombok.extern.slf4j.Slf4j;
+import net.sf.okapi.common.Event;
+import net.sf.okapi.common.EventType;
+import net.sf.okapi.common.exceptions.OkapiIOException;
+import net.sf.okapi.common.filterwriter.GenericContent;
+import net.sf.okapi.common.filterwriter.IFilterWriter;
+import net.sf.okapi.common.resource.RawDocument;
+import net.sf.okapi.common.resource.StartSubDocument;
+import net.sf.okapi.common.resource.TextUnit;
 import net.sf.okapi.filters.ts.TsFilter;
+import org.zanata.exception.FileFormatAdapterException;
+import org.zanata.rest.dto.resource.TextFlowTarget;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
 
 /**
  * Adapter to handle Qt translation (.ts) files.<br/> using the
@@ -38,5 +55,73 @@ public class TSAdapter extends OkapiFilterAdapter {
 
     private static TsFilter prepareFilter() {
         return new TsFilter();
+    }
+
+    @Override
+    protected void generateTranslatedFile(URI originalFile,
+                                          Map<String, TextFlowTarget> translations,
+                                          net.sf.okapi.common.LocaleId localeId, IFilterWriter writer,
+                                          Optional<String> params) {
+        RawDocument rawDoc =
+                new RawDocument(originalFile, "UTF-8",
+                        net.sf.okapi.common.LocaleId.fromString("en"));
+        if (rawDoc.getTargetLocale() == null) {
+            rawDoc.setTargetLocale(localeId);
+        }
+
+        List<String> encounteredIds = new ArrayList<>();
+        try {
+            filter.open(rawDoc);
+            String subDocName = "";
+            while (filter.hasNext()) {
+                Event event = filter.next();
+                if (event.getEventType() == EventType.START_SUBDOCUMENT) {
+                    StartSubDocument startSubDoc =
+                            (StartSubDocument) event.getResource();
+                    subDocName = stripPath(startSubDoc.getName());
+                    writer.handleEvent(event);
+
+                } else if (event.getEventType() == EventType.TEXT_UNIT) {
+                    TextUnit tu = (TextUnit) event.getResource();
+                    if (!tu.getSource().isEmpty() && tu.isTranslatable()) {
+                        String translatable = GenericContent.fromFragmentToLetterCoded(
+                                tu.getSource().getFirstContent(), true);
+                        if (!translatable.isEmpty()) {
+                            TextFlowTarget tft = translations.get(getIdFor(tu, translatable, subDocName));
+                            if (tft != null) {
+                                String id = getIdFor(tu, translatable, subDocName);
+                                if(!encounteredIds.contains(id)) {
+                                    encounteredIds.add(id);
+                                    for (String translated : tft.getContents()) {
+                                        tu.setTargetContent(localeId, GenericContent
+                                                .fromLetterCodedToFragment(translated,
+                                                        tu.getSource().getFirstContent().clone(), true, true));
+                                        writer.handleEvent(event);
+                                        // TODO: Insert a newline
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                } else {
+                    writer.handleEvent(event);
+                }
+            }
+        } catch (OkapiIOException e) {
+            throw new FileFormatAdapterException(
+                    "Unable to generate translated document from original", e);
+        } finally {
+            filter.close();
+            writer.close();
+        }
+    }
+
+    private String stripPath(String name) {
+        if (name.contains("/") && !name.endsWith("/")) {
+            return name.substring(name.lastIndexOf('/') + 1);
+        } else {
+            return name;
+        }
     }
 }
