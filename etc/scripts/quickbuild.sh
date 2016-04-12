a942b335d16b559e6ab766a938236b62d86904fd
Damian Jansen <djansen@redhat.com>
2016-04-12 12:10:54 +1000

    Disallow conflicting -c and -f arguments in quickbuild

--- etc/scripts/quickbuild.sh
+++ etc/scripts/quickbuild.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
 #set -x
 
+function usage {
+  echo ">> Run this script to quickly build the Zanata web archive" >&2
+  echo ">>>> -c Only build GWT components for Chrome (incompatible with -f)" >&2
+  echo ">>>> -f Only build GWT components for Firefox (incompatible with -c)" >&2
+  echo ">>>> Default GWT build is both Chrome and Firefox" >&2
+  exit 0;
+}
+
 gwtMode="-Dchromefirefox"
 while getopts "cfh" opt; do
   case ${opt} in
     c)
+      if [ $gwtMode == "-Dfirefox" ]
+        then usage
+      fi
       gwtMode="-Dchrome"
       echo ">> Building GWT for Google Chrome"
       ;;
     f)
+      if [ $gwtMode == "-Dchrome" ]
+        then usage
+      fi
       gwtMode="-Dfirefox"
       echo ">> Building GWT for Mozilla Firefox"
       ;;
     h)
-      echo ">> Run this script to quickly build the Zanata web archive" >&2
-      echo ">>>> -c Only build GWT components for Chrome" >&2
-      echo ">>>> -f Only build GWT components for Firefox" >&2
-      exit 0;
+      usage
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
