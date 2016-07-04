71311d25af45cf0f88514366a65daf524490b0b1
broccolinisoup <armagan.ersoz@gmail.com>
2016-07-04 13:45:33 +1000

    feat(ZNTA-786) : Continue type safe injection

--- zanata-war/src/test/java/org/zanata/rest/RestLimitingFilterTest.java
+++ zanata-war/src/test/java/org/zanata/rest/RestLimitingFilterTest.java
@@ -6,16 +6,19 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.jglue.cdiunit.deltaspike.SupportDeltaspikeCore;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Answers;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.zanata.ZanataTest;
+import org.zanata.dao.AccountDAO;
 import org.zanata.limits.RateLimitingProcessor;
 import org.zanata.model.HAccount;
+import org.zanata.test.CdiUnitRunner;
 import org.zanata.util.HttpUtil;
 import org.zanata.util.RunnableEx;
 
@@ -26,7 +29,12 @@ import static org.mockito.Mockito.doReturn;
  * @author Patrick Huang <a
  *         href="mailto:pahuang@redhat.com">pahuang@redhat.com</a>
  */
+@RunWith(CdiUnitRunner.class)
+@SupportDeltaspikeCore
 public class RestLimitingFilterTest extends ZanataTest {
+
+    // Using @Inject would be better, but some tests currently require
+    // authenticatedUser to be null, which is difficult without spy()
     private RestLimitingFilter dispatcher;
 
     private static final String API_KEY = "apiKey123";
@@ -41,23 +49,26 @@ public class RestLimitingFilterTest extends ZanataTest {
     private ArgumentCaptor<RunnableEx> taskCaptor;
     @Mock
     private FilterChain filterChain;
+
     private HAccount authenticatedUser;
+
+    @Mock
+    private AccountDAO accountDAO;
 
     private String clientIP = "255.255.0.1";
 
     @Before
     public void beforeMethod() throws ServletException, IOException {
-        MockitoAnnotations.initMocks(this);
         when(request.getMethod()).thenReturn("GET");
         when(request.getHeader(HttpUtil.X_AUTH_TOKEN_HEADER)).thenReturn(
                 API_KEY);
 
-        dispatcher = spy(new RestLimitingFilter(processor));
+        dispatcher = spy(new RestLimitingFilter(processor, accountDAO,
+            authenticatedUser));
 
         // this way we can verify the task actually called super.invoke()
         doNothing().when(filterChain).doFilter(request, response);
         authenticatedUser = null;
-        doReturn(authenticatedUser).when(dispatcher).getAuthenticatedUser();
     }
 
     @Test
