674e28b56ca903e9be36f7eb9aa4c165fa7cc6c5
David Mason <drdmason@gmail.com>
2016-08-24 17:07:39 +1000

    fix(editor): display rejected state colour properly

--- zanata-frontend/src/editor/app/utils/status.js
+++ zanata-frontend/src/editor/app/utils/status.js
@@ -12,6 +12,7 @@ export const STATUS_NEEDS_WORK = 'needswork'
 export const STATUS_NEEDS_WORK_SERVER = 'needreview'
 export const STATUS_TRANSLATED = 'translated'
 export const STATUS_APPROVED = 'approved'
+export const STATUS_REJECTED = 'rejected'
 
 /**
  * Get a string representing the status that should be
@@ -27,8 +28,8 @@ export function defaultSaveStatus (phrase) {
   } else if (hasEmptyTranslation(phrase)) {
     return STATUS_NEEDS_WORK
   } else if (hasTranslationChanged(phrase)) {
-    // TODO also need to handle 'approved' and 'rejected'
-    //      when user is a reviewer and in review mode
+    // TODO may also need to handle 'approved' and 'rejected'
+    //      when user is a reviewer and in review mode.
     return STATUS_TRANSLATED
   } else {
     // TODO when phrase status is a simple value,
