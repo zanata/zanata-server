109fc363b49d0ec814bed84e1eaf9f35813d80ee
kgough <kgough@redhat.com>
2016-09-16 15:58:19 +1000

    replaced instances of diff-delete and diff-insert in functional tests

--- functional-test/src/main/java/org/zanata/page/webtrans/EditorPage.java
+++ functional-test/src/main/java/org/zanata/page/webtrans/EditorPage.java
@@ -532,10 +532,10 @@ public class EditorPage extends BasePage {
         log.info("Query diff from history compare");
         List<String> diffs = new ArrayList<>();
         for (WebElement element : getCompareTabEntries()) {
-            for (WebElement diffElement : element.findElements(By.className("diff-insert"))) {
+            for (WebElement diffElement : element.findElements(By.className("diff-match"))) {
                 diffs.add("++" + diffElement.getText());
             }
-            for (WebElement diffElement : element.findElements(By.className("diff-delete"))) {
+            for (WebElement diffElement : element.findElements(By.className("diff-search"))) {
                 diffs.add("--" + diffElement.getText());
             }
         }
